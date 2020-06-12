e9fc45f01cbc803395af11f6517e15726a589aa8
mvarnar <mvarnar@gmail.com>
2020-06-12 18:01:59 +0500

    implement solveIteratively + tests

--- equation_solver/solver.js
+++ equation_solver/solver.js
@@ -1,14 +1,39 @@
 "use strict";
 
-function solveIteratively(func, eps, initial, maxIterations = 100) {
-    var x = initial;
+function solveIteratively(coefficientMatrix, eps, maxIterations = 10000) {
+    var diagonalElements = [];
+    coefficientMatrix.forEach(function(value, index, matrix) {
+        if (index[0] === index[1]) {
+            diagonalElements.push(value);
+        }
+    });
+
+    coefficientMatrix = coefficientMatrix.map(function (value, index, matrix) {
+        if (index[0] === index[1]) {
+            return 0;
+        }
+        return - (value / diagonalElements[index[0]]);
+    });
+
+    const nVariables = coefficientMatrix.size()[0];
+    const freeCoefficients = coefficientMatrix.subset(math.index(math.range(0, nVariables), nVariables));
+    coefficientMatrix = coefficientMatrix.subset(math.index(math.range(0, nVariables), math.range(0, nVariables)));
+
+    var beta = math.max(math.abs(coefficientMatrix));
+    var eps = (1 - beta) / beta * eps;
+    var x = freeCoefficients;
+    var status = "unsolved";
     for (var i = 0; i < maxIterations; i++) {
         var previousX = x;
-        x = func(previousX);
-        console.log(x);
-        if (Math.abs(x - previousX) < eps) {
+        x = math.add(math.multiply(coefficientMatrix, previousX), freeCoefficients);
+
+        var diff = math.max(math.abs(math.subtract(x, previousX)));
+
+        if ( diff < eps) {
+            status = "solved";
             break;
         }
     }
-    return x;
+
+    return { status: status, x: x };
 }
--- equation_solver/tests/test_solver.js
+++ equation_solver/tests/test_solver.js
@@ -1,64 +1,111 @@
-/// <reference path="../solver.js"/>
+function test(coefficientMatrix, eps, expected) {
+    var result = solveIteratively(coefficientMatrix, eps);
+    var status = result.status;
+    var x = result.x;
 
-describe("solveIteratively", function() {
+    expect(status).toBe("solved");
+    math.abs(math.subtract(x, expected)).forEach(function (diff, index, matrix) {
+        expect(diff < eps).toBe(true);
+    });
+}
 
-    it("sin(x)/x",
+describe("solveIteratively", function () {
+    it("case from the book",
         function () {
-            var func = x => Math.sin(x) / x;
+            var coefficientMatrix = math.matrix([
+                    [20.9, 1.2, 2.1, 0.9, -21.7],
+                    [1.2, 21.2, 1.5, 2.5, -27.46],
+                    [2.1, 1.5, 19.8, 1.3, -28.76],
+                    [0.9, 2.5, 1.3, 32.1, -49.72]
+                ]);
             var eps = 0.001;
-            var exactRoot = 0.87672621539506244597;
+            var expected = math.matrix([ [0.7999], [0.9999], [1.1999], [1.3999] ]);
 
-            var approximateRoot = solveIteratively(func, eps, 1);
-
-            var approximateRootWithinBorders = exactRoot - eps <= approximateRoot && approximateRoot <= exactRoot + eps;
-            expect(approximateRootWithinBorders).toBe(true);
+            test(coefficientMatrix, eps, expected);
         });
 
-    it("(x^3 - 3x^2 - 8)/(-9)",
-        function () {
-            var func = x => (Math.pow(x, 3) - 3 * Math.pow(x, 2) - 8) / (-9);
+    it("case 7",
+        function() {
+            var coefficientMatrix = math.matrix([
+                [-3.1, -1.7, -0.6, 0.9],
+                [-1.7, 3.5, -0.8, -2.6],
+                [0.6, 0.8, -1.9, -1.7]
+            ]);
             var eps = 0.001;
-            var exactRoot = 1.16590558412221;
+            var expected = math.matrix([[0.0608943511], [0.6332642334], [-0.6088694751]]);
 
-            var approximateRoot = solveIteratively(func, eps, 1);
-
-            var approximateRootWithinBorders = exactRoot - eps <= approximateRoot && approximateRoot <= exactRoot + eps;
-            expect(approximateRootWithinBorders).toBe(true);
+            test(coefficientMatrix, eps, expected);
         });
 
-    it("(x^3 - 4) / 6",
-        function () {
-            var func = x => (Math.pow(x, 3) - 4) / 6.0;
-            var eps = 0.001;
-            var exactRoot = -0.7320508075688772935274463415058;
+    it("case 7 eps=0.000001",
+        function() {
+            var coefficientMatrix = math.matrix([
+                [-3.1, -1.7, -0.6, 0.9],
+                [-1.7, 3.5, -0.8, -2.6],
+                [0.6, 0.8, -1.9, -1.7]
+            ]);
+            var eps = 0.000001;
+            var expected = math.matrix([[0.0608943511], [0.6332642334], [-0.6088694751]]);
 
-            var approximateRoot = solveIteratively(func, eps, 1);
-
-            var approximateRootWithinBorders = exactRoot - eps <= approximateRoot && approximateRoot <= exactRoot + eps;
-            expect(approximateRootWithinBorders).toBe(true);
+            test(coefficientMatrix, eps, expected);
         });
 
-    it("(x^3 + 2) / (-4)",
-        function () {
-            var func = x => (Math.pow(x, 3) + 2) / (-4);
+    it("case when beta > 1/2",
+        function() {
+            var coefficientMatrix = math.matrix([
+                [2, 1.5, -1],
+                [1.6, 2, -2],
+            ]);
             var eps = 0.001;
-            var exactRoot = -0.473465807729126;
+            var expected = math.matrix([[-0.625], [1.5]]);
 
-            var approximateRoot = solveIteratively(func, eps, 1);
-
-            var approximateRootWithinBorders = exactRoot - eps <= approximateRoot && approximateRoot <= exactRoot + eps;
-            expect(approximateRootWithinBorders).toBe(true);
+            test(coefficientMatrix, eps, expected);
         });
 
-    it("(x^4 - 4) / 4",
-        function () {
-            var func = x => (Math.pow(x, 4) - 4) / 4;
+    it("case with one variable",
+        function() {
+            var coefficientMatrix = math.matrix([
+                [2, 1]
+            ]);
             var eps = 0.001;
-            var exactRoot = -0.861982568269307;
+            var expected = math.matrix([[-0.5]]);
 
-            var approximateRoot = solveIteratively(func, eps, 1);
-            
-            var approximateRootWithinBorders = exactRoot - eps <= approximateRoot && approximateRoot <= exactRoot + eps;
-            expect(approximateRootWithinBorders).toBe(true);
+            test(coefficientMatrix, eps, expected);
+        });
+
+    it("case with zero free coefficients",
+        function () {
+            var coefficientMatrix = math.matrix([
+                [2, 1, 0],
+                [1, 2, 0]
+            ]);
+            var eps = 0.001;
+            var expected = math.matrix([[0], [0]]);
+
+            test(coefficientMatrix, eps, expected);
+        });
+
+    it("case with dependent row",
+        function () {
+            var coefficientMatrix = math.matrix([
+                [2, 1, 0],
+                [4, 2, 0]
+            ]);
+            var eps = 0.001;
+
+            var status = solveIteratively(coefficientMatrix, eps).status;
+            expect(status).toBe("unsolved");
+        });
+
+    it("case with diagonal elements only",
+        function () {
+            var coefficientMatrix = math.matrix([
+                [1, 0, 2],
+                [0, 1, 3]
+            ]);
+            var eps = 0.001;
+            var expected = math.matrix([[-2], [-3]]);
+
+            test(coefficientMatrix, eps, expected);
         });
 });
