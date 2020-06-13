32e7d099f4ecb588a890cf113c4a79de143c1716
mvarnar <mvarnar@gmail.com>
2020-06-13 19:44:45 +0500

    refactoring

--- solver.js
+++ solver.js
@@ -1,43 +1,64 @@
 "use strict";
 
 function solveIteratively(coefficientMatrix, eps, maxIterations = 10000) {
-    var diagonalElements = [];
-    coefficientMatrix.forEach(function(value, index, matrix) {
+    var diagonalElements = _getDiagonalElements(coefficientMatrix);
+    coefficientMatrix = _moveOutDiagonalElements(coefficientMatrix, diagonalElements);
+
+    let coefficientsOfVariables;
+    let freeCoefficients;
+    [coefficientsOfVariables, freeCoefficients] = _splitCoefficients(coefficientMatrix);
+
+    const beta = math.max(math.abs(coefficientsOfVariables));
+    const betaEps = (1 - beta) / beta * eps;
+
+    return _solveIteratively(coefficientsOfVariables, freeCoefficients, betaEps, maxIterations);
+}
+
+function _solveIteratively(coefficientsOfVariables, freeCoefficients, eps, maxIterations) {
+    var x = freeCoefficients;
+    var status = "unsolved";
+    for (let i = 0; i < maxIterations; i++) {
+        const previousX = x;
+        x = math.add(math.multiply(coefficientsOfVariables, previousX), freeCoefficients);
+
+        const diff = math.max(math.abs(math.subtract(x, previousX)));
+
+        if (diff < eps) {
+            status = "solved";
+            break;
+        }
+    }
+
+    // из-за особенностей работы функции subset в библиотеке math.js, 
+    // в переменной x окажется число, если параметр coefficientMatrix был размерности [1, 1]
+    x = typeof x == "number" ? math.matrix([[x]]) : x;
+    return { status: status, x: x }
+}
+
+function _getDiagonalElements(matrix) {
+    const diagonalElements = [];
+    matrix.forEach(function (value, index, matrix) {
         if (index[0] === index[1]) {
             diagonalElements.push(value);
         }
     });
+    return diagonalElements;
+}
 
-    coefficientMatrix = coefficientMatrix.map(function(value, index, matrix) {
+function _moveOutDiagonalElements(matrix, diagonalElements){
+    return matrix.map(function (value, index, matrix) {
         if (index[0] === index[1]) {
             return 0;
         }
         return - (value / diagonalElements[index[0]]);
     });
+}
 
-    const nVariables = coefficientMatrix.size()[0];
+function _splitCoefficients(matrix) {
+    const nVariables = matrix.size()[0];
     const lastColumnIndex = math.index(math.range(0, nVariables), nVariables);
-    const freeCoefficients = coefficientMatrix.subset(lastColumnIndex);
+    const freeCoefficients = matrix.subset(lastColumnIndex);
     const exceptLastColumnIndex = math.index(math.range(0, nVariables), math.range(0, nVariables));
-    coefficientMatrix = coefficientMatrix.subset(exceptLastColumnIndex);
-
-    const beta = math.max(math.abs(coefficientMatrix));
-    const betaEps = (1 - beta) / beta * eps;
-    var x = freeCoefficients;
-    var status = "unsolved";
-    for (let i = 0; i < maxIterations; i++) {
-        const previousX = x;
-        x = math.add(math.multiply(coefficientMatrix, previousX), freeCoefficients);
-
-        const diff = math.max(math.abs(math.subtract(x, previousX)));
-
-        if (diff < betaEps) {
-            status = "solved";
-            break;
-        }
-    }
-    // из-за особенностей работы функции subset в библиотеке math.js, 
-    // в переменной x окажется число, если параметр coefficientMatrix был размерности [1, 1]
-    x = typeof x == "number" ? math.matrix([[x]]) : x;
-    return { status: status, x: x };
+    const coefficientsOfVariables = matrix.subset(exceptLastColumnIndex);
+    return [coefficientsOfVariables, freeCoefficients];
 }
