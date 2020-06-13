ede4572873bd5849b84410eefa9c9f0d5dd1df25
mvarnar <mvarnar@gmail.com>
2020-06-13 14:43:36 +0500

    refactoring

--- equation_solver/solver.js
+++ equation_solver/solver.js
@@ -8,7 +8,7 @@ function solveIteratively(coefficientMatrix, eps, maxIterations = 10000) {
         }
     });
 
-    coefficientMatrix = coefficientMatrix.map(function (value, index, matrix) {
+    coefficientMatrix = coefficientMatrix.map(function(value, index, matrix) {
         if (index[0] === index[1]) {
             return 0;
         }
@@ -16,20 +16,22 @@ function solveIteratively(coefficientMatrix, eps, maxIterations = 10000) {
     });
 
     const nVariables = coefficientMatrix.size()[0];
-    const freeCoefficients = coefficientMatrix.subset(math.index(math.range(0, nVariables), nVariables));
-    coefficientMatrix = coefficientMatrix.subset(math.index(math.range(0, nVariables), math.range(0, nVariables)));
+    const lastColumnIndex = math.index(math.range(0, nVariables), nVariables);
+    const freeCoefficients = coefficientMatrix.subset(lastColumnIndex);
+    const exceptLastColumnIndex = math.index(math.range(0, nVariables), math.range(0, nVariables));
+    coefficientMatrix = coefficientMatrix.subset(exceptLastColumnIndex);
 
-    var beta = math.max(math.abs(coefficientMatrix));
-    var eps = (1 - beta) / beta * eps;
+    const beta = math.max(math.abs(coefficientMatrix));
+    eps = (1 - beta) / beta * eps;
     var x = freeCoefficients;
     var status = "unsolved";
-    for (var i = 0; i < maxIterations; i++) {
-        var previousX = x;
+    for (let i = 0; i < maxIterations; i++) {
+        const previousX = x;
         x = math.add(math.multiply(coefficientMatrix, previousX), freeCoefficients);
 
-        var diff = math.max(math.abs(math.subtract(x, previousX)));
+        const diff = math.max(math.abs(math.subtract(x, previousX)));
 
-        if ( diff < eps) {
+        if (diff < eps) {
             status = "solved";
             break;
         }
